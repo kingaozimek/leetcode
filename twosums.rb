8d68a2135ee6385f9224a9291af8038c0af11834
Kinga Ozimek <kingaozime@gmail.com>
2022-05-29 18:13:50 +0700

    challeng1

---
+++ twosums.rb
@@ -0,0 +1,18 @@
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+
+def two_sum(nums, target)
+  checked_numbers = {}
+
+ nums.each_with_index do |value, i|
+   difference = target - value
+
+   if checked_numbers[difference]
+     return [checked_numbers[difference], i]
+   else
+    checked_numbers[value] = i
+   end
+ end
+end
